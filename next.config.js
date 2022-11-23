08944fd96f360255c73ea4595e8a98b5f756ac0b
donghyunKang-dev <make_w@naver.com>
2022-11-23 17:29:23 +0900

    Feat: Set i18n config
    defaultLocale: ko로 설정합니다.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 
+const { i18n } = require('./next-i18next.config')
+
 const nextConfig = {
   // reactStrictMode: true,
   swcMinify: true,
@@ -14,6 +16,7 @@ const nextConfig = {
 
     return config
   },
+  i18n,
 }
 
 module.exports = nextConfig
