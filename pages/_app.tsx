df809faa685021ae800e960117f52ff171dbef47
donghyunKang-dev <make_w@naver.com>
2022-11-23 10:10:43 +0900

    Feat: Set recoil
    client side state를 다루기 위해 recoil을 채택합니다.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 import type { AppProps } from 'next/app'
 
+import { RecoilRoot } from 'recoil'
+
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from 'styles/globalstyles'
 import theme from 'styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <RecoilRoot>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </RecoilRoot>
   )
 }
