418747238ad305beed1606e5660226bf7a531bde
donghyunKang-dev <make_w@naver.com>
2022-11-24 10:30:45 +0900

    Docs: Create ui folder
    components 폴더 내 폴더구조 변경

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,8 +7,8 @@ import { useRouter } from 'next/router'
 import { useTranslation } from 'react-i18next'
 
 import MenuIcon from 'public/image/menu.svg'
-import DropDown from 'components/DropDown/DropDown'
-import Toggle from '@components/Toggle'
+import DropDown from 'components/ui/DropDown'
+import Toggle from 'components/ui/Toggle'
 
 import * as S from './Header.styled'
 
